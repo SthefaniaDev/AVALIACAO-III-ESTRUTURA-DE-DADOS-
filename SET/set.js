26778d57ddd34679958c276d8e99c771dad9cb35
Carla ⋆.˚✮🎧✮˚.⋆ <sthefania.ferreira.dev@gmail.com>
2026-01-13 18:41:46 -0300

    Implementação do método has()

--- SET/set.js
+++ SET/set.js
@@ -2,5 +2,10 @@ module.exports = class Set {
     constructor() {
         this.items = {};
         this.count = 0;
+       }
+
+       has(element) {
+        return Object.prototype.hasOwnProperty.call(this.items, element);
+        // Usa o método hasOwnProperty original do JavaScript para verificar, de forma segura, se o objeto items possui a chave informada.
     }
 }
