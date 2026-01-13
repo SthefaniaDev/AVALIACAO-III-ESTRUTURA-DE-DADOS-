8da903d47aadd2e1f417cf968d19767214192dc2
Carla ⋆.˚✮🎧✮˚.⋆ <sthefania.ferreira.dev@gmail.com>
2026-01-13 18:44:52 -0300

    Implementação do método delete()

--- SET/set.js
+++ SET/set.js
@@ -17,4 +17,13 @@ module.exports = class Set {
         }
         return false; // Indica que a adição não foi realizada, pois o elemento já existe
     }
+
+    delete(element){
+        if(this.has(element)){ // Verifica, utilizando a função has(), se o elemento já existe no conjunto
+            delete this.items[element];  //Caso exista, o elemento é removido do conjunto
+            this.count --; //Decrementa o contador de elementos
+            return true; // indica que a remoção foi realizada com sucesso
+        }
+        return false; // Indica que a ação não foi realizada, pois o elemento não existe
+    }
 }
