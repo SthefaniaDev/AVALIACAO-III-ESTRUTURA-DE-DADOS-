7d6b59ff5d1aa8ae37cdc6a0e374f6ade9fb9fa6
f3lipecostadev <f3lipecosta.dev@gmail.com>
2026-01-13 19:04:14 -0300

    Implementação método hasKey()

--- DICTIONARY/dictionary.js
+++ DICTIONARY/dictionary.js
@@ -2,4 +2,9 @@ module.exports = class Dicionary{
     constructor(){
         this.table = {};
     }
+
+    /*Verifica se uma chave existe no dicionário */ 
+    hasKey(key){
+        return Object.prototype.hasOwnProperty.call(this.table, key);
+    }
 }
