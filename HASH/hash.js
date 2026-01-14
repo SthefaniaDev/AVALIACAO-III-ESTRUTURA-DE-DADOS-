fca7746dc225207ee79ff9d5e879cf23e7ff43dd
Alexandre <martinssilvaa38@gmail.com>
2026-01-13 22:45:18 -0300

    criação do método hash(key)

--- HASH/hash.js
+++ HASH/hash.js
@@ -2,4 +2,12 @@ module.exports = class HashTable {
   constructor() {
     this.table = {}; // como um armario
   }
+
+  hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i); // 3. Pega o "valor" de cada letra do nome e soma.
+    }
+    return hash % 37; // 4. Divide por 37 e pega o resto. O endereço será sempre entre 0 e 36.
+  }
 };
