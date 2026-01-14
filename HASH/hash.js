a20f3d8952fc909948b7f326b6591a984e27725e
Alexandre <martinssilvaa38@gmail.com>
2026-01-13 22:59:01 -0300

    finalização do método put(key, value) -> possui o key e o value, agora está completa

--- HASH/hash.js
+++ HASH/hash.js
@@ -16,5 +16,15 @@ module.exports = class HashTable {
     if (!this.table[position]) {
       this.table[position] = []; //  Se a gaveta estiver vazia, coloca um saquinho lá dentro.
     }
+    // 7. Se já tiver algo no saquinho, ele olha item por item.
+    for (let i = 0; i < this.table[position].length; i++) {
+      if (this.table[position][i][0] === key) {
+        // 8. Se achar o mesmo nome...
+        this.table[position][i][1] = value; // 9. Só troca o valor antigo pelo novo.
+        return;
+      }
+    }
+    // 10. Se não achou o nome no saquinho, joga o novo par [nome, valor] lá dentro.
+    this.table[position].push([key, value]);
   }
 };
