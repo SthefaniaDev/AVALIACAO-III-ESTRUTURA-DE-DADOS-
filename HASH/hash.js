530acaf44d1ea2ed2aafcf5ab22bb3fb8152d952
Alexandre <martinssilvaa38@gmail.com>
2026-01-13 22:47:25 -0300

    inicio da criação do método put(key, value) -> só possui o key

--- HASH/hash.js
+++ HASH/hash.js
@@ -6,8 +6,15 @@ module.exports = class HashTable {
   hash(key) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
-      hash += key.charCodeAt(i); // 3. Pega o "valor" de cada letra do nome e soma.
+      hash += key.charCodeAt(i); //  Pega o "valor" de cada letra do nome e soma.
     }
-    return hash % 37; // 4. Divide por 37 e pega o resto. O endereço será sempre entre 0 e 36.
+    return hash % 37; //  Divide por 37 e pega o resto. O endereço será sempre entre 0 e 36.
+  }
+  put(key, value) {
+    const position = this.hash(key); // Pergunta pra máquina: "Em qual gaveta guardo isso?"
+
+    if (!this.table[position]) {
+      this.table[position] = []; //  Se a gaveta estiver vazia, coloca um saquinho lá dentro.
+    }
   }
 };
